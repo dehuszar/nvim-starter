5849fddc061d06ebee9878c0baf64822d1159a44
Heiker Curiel <heikercuriel@gmail.com>
2023-10-05 19:00:50 -0400

    feat: add new commands

--- plugin/commands.lua
+++ plugin/commands.lua
@@ -5,6 +5,33 @@ require('user.buffer-nav').setup()
 
 vim.api.nvim_create_user_command('ReloadConfig', 'source $MYVIMRC', {})
 
+vim.api.nvim_create_user_command(
+  'GetSelection',
+  function()
+    local f = vim.fn
+    local temp = f.getreg('s')
+    vim.cmd('normal! gv"sy')
+
+    f.setreg('/', f.escape(f.getreg('s'), '/'):gsub('\n', '\\n'))
+
+    f.setreg('s', temp)
+  end,
+  {desc = 'Add selected text to the search register'}
+)
+
+vim.api.nvim_create_user_command(
+  'TrailspaceTrim',
+  function()
+    -- Save cursor position to later restore
+    local curpos = vim.api.nvim_win_get_cursor(0)
+
+    -- Search and replace trailing whitespace
+    vim.cmd([[keeppatterns %s/\s\+$//e]])
+    vim.api.nvim_win_set_cursor(0, curpos)
+  end,
+  {desc = 'Delete extra whitespace'}
+)
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = 'user_cmds',
   desc = 'Highlight on yank',
